ba5328507d2ebccbff38e2c4bc5a3fb2ac97e2ea
djoyman <djoyman@tula.net>
2020-05-07 18:04:58 +0300

    initial project

---
+++ app/src/main/java/test/app/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package test.app.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btn1_Click(View V) {
+        Intent intent = new Intent(MainActivity.this, NewWindow.class);
+        startActivity(intent);
+    }
+
+    public void btn2_Click(View V) {
+        Intent intent = new Intent(MainActivity.this, SettingsWindow.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/test/app/myapplication/NewWindow.java
@@ -0,0 +1,14 @@
+package test.app.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_window);
+    }
+}
